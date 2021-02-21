61d29342d802719c0039bfa30e3fd8751c7683f9
baranov-ivan <vano95by@gmail.com>
2021-02-21 16:26:58 +0300

    feat: add brackets task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,56 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let openBrackets = [];
+    let closeBrackets = [];
+    let sameBrackets = [];
+
+    bracketsConfig.forEach((item) => {
+        if (item[0] === item[1]) {
+            sameBrackets.push(item[0]);
+        } else {
+            openBrackets.push(item[0]);
+            closeBrackets.push(item[1]);
+        }
+    });
+
+    let strArray = str.split("");
+
+    let openStack = [];
+    let stackChanged = false;
+
+    for (let i = 0; i < strArray.length; i++) {
+        if (openBrackets.indexOf(strArray[i]) !== -1) {
+            openStack.push(strArray[i]);
+            stackChanged = true;
+        } else if (closeBrackets.indexOf(strArray[i]) !== -1) {
+            if (openStack.length > 0) {
+                if (
+                    openBrackets.indexOf(openStack[openStack.length - 1]) ===
+                    closeBrackets.indexOf(strArray[i])
+                ) {
+                    openStack.pop();
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        } else if (sameBrackets.indexOf(strArray[i]) !== -1) {
+            if(openStack.includes(strArray[i])) {
+                if (openStack[openStack.length - 1] === strArray[i]) {
+                    openStack.pop();
+                } else {
+                    return false;
+                }
+            } else {
+                openStack.push(strArray[i]);
+                stackChanged = true;
+            }
+        }
+    }
+
+    if(openStack.length === 0 && stackChanged === true) {
+        return true;
+    }
+
+    return false;
 }
